2b991b7006870e836166b0817e0dd981f4d36892
Sanababayan <babayansana@gmail.com>
2023-05-05 10:18:13 +0430

    comit before push

--- StrongholdCrusader/src/main/java/model/buildings/Manufacturer.java
+++ StrongholdCrusader/src/main/java/model/buildings/Manufacturer.java
@@ -4,7 +4,6 @@ import model.User;
 
 public class Manufacturer extends Producer {
 
-
     public Manufacturer(String name, User owner,int productionRate,int capacity) {
         super(name, owner, productionRate, capacity);
     }
