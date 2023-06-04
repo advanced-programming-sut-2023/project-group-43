bdbbc8b451dedfb3872f40a8f5e8d04b14dc727d
ID <115931028+AidaJalali@users.noreply.github.com>
2023-06-04 20:29:07 +0330

    complete Store menu

--- StrongholdCrusader/src/main/java/view/StoreTable.java
+++ StrongholdCrusader/src/main/java/view/StoreTable.java
@@ -4,7 +4,6 @@ import controller.GameControllers.StoreController;
 import enums.ImageEnum;
 import enums.environmentEnums.Material;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,20 +11,18 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
-import model.DataBase;
 
-import java.net.URL;
 import java.util.ArrayList;
 
 public class StoreTable extends Application {
 
     private static Stage stage;
+    public final String css = this.getClass().getResource("/css/style.css").toExternalForm();
     public BorderPane root = new BorderPane();
     public BorderPane center = new BorderPane();
     private Scene scene;
     private HBox hBox = new HBox();
     private HBox images = new HBox();
-    public final String css = this.getClass().getResource("/css/style.css").toExternalForm();
     private String item;
     private ArrayList<Material> materials;
     private StoreController storeController;
@@ -97,50 +94,76 @@ public class StoreTable extends Application {
     }
 
     private void createPopUp(Material material) {
+        System.out.println("I am creating pop up");
         Popup popup = new Popup();
 
         HBox main = new HBox();
-        root.setBackground(new Background(new BackgroundImage(ImageEnum.OLD_PAPER.getImage(),
+        main.setMinSize(200, 200);
+        main.setBackground(new Background(new BackgroundImage(ImageEnum.OLD_PAPER.getImage(),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(1, 1, true, true, false, false))));
 
         BorderPane right = new BorderPane();
         ImageView imageView = new ImageView();
         imageView.setImage(ImageEnum.COIN.getImage());
+        imageView.setFitWidth(80);
+        imageView.setFitHeight(80);
         Label coin = new Label();
-        coin.setText(String.valueOf(DataBase.getInstance().findLoggedInUser().getGovernance().getGold()));
-        right.setCenter(imageView);
-        right.setBottom(coin);
+        coin.setMinSize(100, 100);
+        //TODO  2 MUST CHANGED TO USER DATA BASE
+        coin.setText(String.valueOf(2));
+        right.setBottom(imageView);
+        right.setRight(coin);
 
         BorderPane left = new BorderPane();
         ImageView pic = new ImageView();
         pic.setImage(material.getImage());
+        pic.setFitWidth(80);
+        pic.setFitHeight(80);
         Label number = new Label();
+        number.setMinSize(100, 100);
         number.setText(String.valueOf(material.getRange()));
-        left.setCenter(pic);
-        left.setBottom(number);
+        left.setBottom(pic);
+        left.setRight(number);
 
         BorderPane center = new BorderPane();
         VBox vBox = new VBox();
         Button buy = new Button("Buy  " + material.getBuyingPrice());
         Button sell = new Button("Sell " + material.getSellingPrice());
-        Button back = new Button("Back") ;
-        sell.setOnAction(actionEvent -> sell(material));
-        buy.setOnAction(actionEvent -> buy(material));
+        Button back = new Button("Back");
+        buy.setMinWidth(100);
+        sell.setMinWidth(100);
+        back.setMinWidth(100);
+        sell.setOnAction(actionEvent -> sell(material, number, coin));
+        buy.setOnAction(actionEvent -> buy(material, number, coin));
         back.setOnAction(actionEvent -> popup.hide());
         vBox.getChildren().addAll(buy, sell, back);
+        vBox.setSpacing(60);
         center.setCenter(vBox);
 
         main.setBackground(new Background(new BackgroundImage(ImageEnum.OLD_PAPER.getImage(),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(1, 1, true, true, false, false))));
 
-        main.getChildren().addAll(right, left, center);
+        main.getChildren().addAll(right, center, left);
+        main.setSpacing(40);
+        main.setTranslateX(20);
+
+        popup.getContent().add(main);
+        popup.show(stage);
     }
 
-    private void buy(Material material){
-
+    private void buy(Material material, Label number, Label coin) {
+        storeController.buy(material.getName(), 1);
+        number.setText(String.valueOf(material.getRange()));
+        coin.setText("-1");
+        //TODO
     }
 
-    private void sell(Material material){}
+    private void sell(Material material, Label number, Label coin) {
+        storeController.sell(material.getName(), 1);
+        number.setText(String.valueOf(material.getRange()));
+        coin.setText("-1");
+        //TODO
+    }
 
 }
 
