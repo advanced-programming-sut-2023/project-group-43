d9ef519b038430eff67af7cebaa9c6c47f135570
asalmskin <asalmsk82@gmail.com>
2023-07-04 22:24:07 +0330

    complete chat class and add chat enum

--- phase3/Server/src/main/java/model/Chat.java
+++ phase3/Server/src/main/java/model/Chat.java
@@ -1,13 +1,39 @@
 package model;
 
+import enums.ChatType;
+
 import java.util.ArrayList;
 
 public class Chat {
+
+    private String name;
+    private ChatType type;
     private ArrayList<Message> messages = new ArrayList<>();
     private ArrayList<User> members = new ArrayList<>();
 
+    public Chat(String name, ChatType chatType) {
+        this.name = name;
+        this.type = chatType;
+    }
+
     public ArrayList<Message> getMessages() {
         return messages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ChatType getType() {
+        return type;
+    }
+
+    public void setType(ChatType type) {
+        this.type = type;
     }
 
     public void setMessages(ArrayList<Message> messages) {
