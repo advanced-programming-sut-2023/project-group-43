3456572efcaca27dce6a17eb23a3febb81ac47bc
asalmskin <asalmsk82@gmail.com>
2023-07-05 13:35:19 +0330

    add room to chatroom

--- phase3/Client/src/main/java/view/ChatroomMenu.java
+++ phase3/Client/src/main/java/view/ChatroomMenu.java
@@ -1,19 +1,39 @@
 package view;
 
+import com.google.gson.Gson;
+import enums.ChatType;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import model.Chat;
+import model.DataBase;
+import model.User;
+import network.Client;
+import network.NotificationReceiver;
+import network.Packet;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class ChatroomMenu extends Application {
 
+    public ChoiceBox rooms;
+    public TextArea members;
+    public TextField roomName;
     private Scene scene;
     @FXML
     private Pane mainPane;
@@ -29,6 +49,17 @@ public class ChatroomMenu extends Application {
         stage.show();
     }
 
+    @FXML
+    public void initialize() {
+        ObservableList<String> list = FXCollections.observableArrayList();
+        for (Chat chat: NotificationReceiver.getChats()) {
+            if (chat.getChatType().equals(ChatType.GROUP))
+                list.add(chat.getName());
+        }
+        rooms.setItems(list);
+        rooms.setValue("none");
+    }
+
     private void setBackground() {
         mainPane.setBackground(new Background(new BackgroundImage(new Image(ProfileMenu.class.getResource("/images/background/5559468.jpg").toExternalForm()),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(1, 1, true, true, false, false))));
@@ -39,5 +70,43 @@ public class ChatroomMenu extends Application {
         ChatMenu chatMenu = new ChatMenu();
         chatMenu.setName("public");
         chatMenu.start(RegisterMenu.getStage());
+    }
+
+    public void back() throws Exception {
+        (new MainMenu()).start(RegisterMenu.getStage());
+    }
+
+    public void enterRoom() throws Exception {
+        ChatMenu chatMenu = new ChatMenu();
+        chatMenu.setName(rooms.getValue().toString());
+        chatMenu.start(RegisterMenu.getStage());
+    }
+
+    public void makeNewRoom() throws Exception {
+        Scanner scanner = new Scanner(members.getText());
+        ArrayList<User> members = new ArrayList<>();
+        boolean isPossible = true;
+        members.add(DataBase.getInstance().getUserByUsername(MainMenu.getUsername()));
+        while (scanner.hasNextLine()) {
+            User user = DataBase.getInstance().getUserByUsername(scanner.nextLine());
+            if (user == null) isPossible = false;
+            else members.add(user);
+        }
+        if (roomName.getText().isEmpty()) isPossible = false;
+        if (isPossible) {
+            Chat chat = new Chat();
+            chat.setName(roomName.getText());
+            chat.setChatType(ChatType.GROUP);
+            chat.setMembers(members);
+            Client.dataOutputStream.writeUTF(new Packet("update chat", (new Gson()).toJson(chat)).toJson());
+            ChatMenu chatMenu = new ChatMenu();
+            chatMenu.setName(roomName.getText());
+            chatMenu.start(RegisterMenu.getStage());
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("users are invalid or room name is empty");
+            alert.show();
+        }
     }
 }
